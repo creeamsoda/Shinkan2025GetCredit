9d748a868ac0d52b893290088b26785d1877e805
creeamsoda <sotakurisu@gmail.com>
2025-04-17 12:55:49 +0900

    不要なファイルを削除

--- ShinkanBakaGame/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ShinkanBakaGame.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
